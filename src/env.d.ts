0bbc6ae449768c528529f27fe2f02f9f3eb64785
mosesy5688-cell <238160801+mosesy5688-cell@users.noreply.github.com>
2025-11-08 19:01:54 +0800

    feat: Implement Cloudflare runtime type safety

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,14 @@
-/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+type KVNamespace = import('@cloudflare/workers-types').KVNamespace;
+
+type Runtime = import('@astrojs/cloudflare').Runtime<
+  {
+    // Add binding for your KV namespace here.
+    AI_NEXUS_KV: KVNamespace;
+  }
+>;
+
+declare namespace App {
+  interface Locals extends Runtime {}
+}
