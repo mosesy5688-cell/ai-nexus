14566076fc88aaa81b57438fe8b7d8ea4449fbdf
mosesy5688-cell <238160801+mosesy5688-cell@users.noreply.github.com>
2025-11-10 22:21:29 +0800

    feat: Enhance frontend UI and add .gitattributes

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
+["src/content/keywords/stable-diffusion/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fkeywords%2Fstable-diffusion%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/keywords/code-generation/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fkeywords%2Fcode-generation%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/keywords/text-to-speech/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fkeywords%2Ftext-to-speech%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/keywords/stable-diffusion/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fkeywords%2Fstable-diffusion%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/keywords/image-generation/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fkeywords%2Fimage-generation%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
