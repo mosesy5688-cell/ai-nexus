f3a83eeda0dad25871354e6d0a4366a33c93dfa7
mosesy5688-cell <238160801+mosesy5688-cell@users.noreply.github.com>
2025-11-16 08:21:32 +0800

    Final Fix: Astro2.__renderMarkdown is not a function

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import rss from "@astrojs/rss";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import path from 'path';
@@ -12,7 +14,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
 export default defineConfig({
   site: 'https://free2aitools.com',
-  integrations: [react(), tailwind(), sitemap(), icon()],
+  integrations: [react(), tailwind(), mdx(), rss(), sitemap(), icon()],
   vite: {
     resolve: {
       alias: {
