3ab5f180d2d3d1e74cdeeddd17ba769cee9c1a92
mosesy5688-cell <238160801+mosesy5688-cell@users.noreply.github.com>
2026-02-17 00:34:04 +0800

    fix: resolve isSatellite ReferenceError in aggregator.js

--- scripts/factory/aggregator.js
+++ scripts/factory/aggregator.js
@@ -74,6 +74,8 @@ async function main() {
     // Note: If allEntities is empty, we MUST have shards to proceed.
 
     let successCount = 0;
+    const isSatellite = !!taskArg && taskArg !== 'core' && taskArg !== 'health';
+
     if (isSatellite) {
         fullSet = allEntities;
     } else {
